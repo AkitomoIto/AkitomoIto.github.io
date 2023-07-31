9bd1dba2a2a3c9342e963fe74f3baada16a7961a
AkitomoIto <135773009+AkitomoIto@users.noreply.github.com>
2023-08-01 07:44:44 +0900

    Add files via upload

---
+++ pokemonImage.js
@@ -0,0 +1,18 @@
+const allPokemon = [
+    {
+        name : "Charizard",
+        url : "https://www.pokemon-card.com/assets/images/card_images/large/CP6/032217_P_RIZADON.jpg",
+    },
+    {
+        name : "Blastoise",
+        url : "https://www.pokemon-card.com/ex/25th/assets/images/s8aP/card-3.png",
+    },
+    {
+        name : "Venusaur",
+        url : "https://www.pokemon-card.com/ex/25th/assets/images/s8aP/card-2.png",
+    },
+    {
+        name : "pikachu",
+        url : "https://www.pokemon-card.com/assets/images/card_images/large/S8a/040066_P_PIKACHIXYUU.jpg",
+    }
+]
