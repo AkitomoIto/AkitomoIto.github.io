f58a4cbff2a40b6f44dbf59adf304ea58fa6f93b
AkitomoIto <135773009+AkitomoIto@users.noreply.github.com>
2023-08-01 07:05:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-'use strict'
-// 1行目に記載している 'use strict' は削除しないでください
 
+function funcPokemon (){
+  const name = document.getElementById("PokemonSelect").value;
+  // console.log(name);
+  // console.log(allPokemon[name]);
+  for (const element of allPokemon){
+    // console.log(element["name"])
+    if (element["name"]=== name ){
+      const pokemonUrl = element["url"]
+      console.log(pokemonUrl);
+      document.getElementById("pokeCard").src = pokemonUrl;
+    }
+  }
+  
+}
