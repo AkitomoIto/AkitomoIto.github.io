1f2d090be05a38c30439a810d235ceb946133e68
AkitomoIto <135773009+AkitomoIto@users.noreply.github.com>
2023-08-02 08:19:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 
 function funcPokemon (){
   const name = document.getElementById("PokemonSelect").value;
-  // console.log(name);
-  // console.log(allPokemon[name]);
   for (const element of allPokemon){
-    // console.log(element["name"])
     if (element["name"]=== name ){
       const pokemonUrl = element["url"]
-      console.log(pokemonUrl);
       document.getElementById("pokeCard").src = pokemonUrl;
     }
   }
-  
 }
